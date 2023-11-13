f9cb2fa602b5c2e3885bd0e842f9006538417b9e
sumukh <50351727+skywalker5014@users.noreply.github.com>
2023-11-13 20:57:53 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,14 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <script async src=`"https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}"`></script>
-<script>
+    <Script async src=`"https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}"`></Script>
+<Script>
+    {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', `${process.env.GOOGLE_ANALYTICS_ID}`);
-</script>
+  gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
+`}
+</Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
